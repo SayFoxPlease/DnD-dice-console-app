6689e5fa0336933911e615bbbea5905ff8d6cce0
SayFoxPlease <36195241+SayFoxPlease@users.noreply.github.com>
2021-04-25 21:52:25 +0300

    Add files via upload

---
+++ const.py
@@ -0,0 +1,17 @@
+# Списки
+
+dice_list = (4, 6, 8, 10, 12, 20, 100,)
+roll_mod_list = (1, 2, 3)
+
+# Переменные
+
+global dice
+global mod
+global result
+
+# Сообщения
+
+welcome = 'reD & Dice'
+type_dice = 'Какой дайс?\n:'
+mode_dice = 'Выберете одно из трех:\n1 - Ничего\n2 - Преимущество\n3 - Помеха\n:'
+error_message = 'Что-то пошло не так'
---
+++ main.py
@@ -0,0 +1,39 @@
+from random import randint
+import const
+
+
+def input_dices():
+    const.dice = input(const.type_dice)
+    const.mod = input(const.mode_dice)
+
+
+def normal_roll():
+    const.result = int(randint(1, int(const.dice)))
+
+
+def advantage():
+    const.result = min(int(randint(1, int(const.dice))), int(randint(1, int(const.dice))))
+
+
+def hindrance():
+    const.result = max(int(randint(1, int(const.dice))), int(randint(1, int(const.dice))))
+
+
+def main():
+    print(const.welcome)
+    input_dices()
+    if int(const.dice) in const.dice_list and int(const.mod) in const.roll_mod_list:
+        if const.mod == '1':
+            normal_roll()
+        elif const.mod == '2':
+            hindrance()
+        elif const.mod == '3':
+            advantage()
+    else:
+        print(const.error_message)
+    print('Выпало ' + str(const.result) + '!\n')
+
+
+if __name__ == "__main__":
+    while True:
+        main()
